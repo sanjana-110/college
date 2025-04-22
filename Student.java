f837da29f58c83edabdc5160f4381ab0e723811c
sanjana-110 <sanjanawadeyar500@gmail.com>
2025-04-22 14:25:08 +0530

    save this file

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+    public static void main(String[] args) {
+        String name="sanjana";
+        int id =110;
+        String course ="devops";
+        String branch="Bca";
+        
+        System.out.println("student Name:"+name);
+        System.out.println("student id:"+id);
+        System.out.println("student course:"+course);
+        System.out.println("student branch"+branch);
+
+    }
+}
