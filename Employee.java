12d46fc00d226a2520b83e1ccb950f26b46cc058
sanjana-110 <sanjanawadeyar500@gmail.com>
2025-04-22 15:57:25 +0530

    save this file

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    public static void main(String[] args) {
+        String name="sanjana";
+        int id =110;
+        
+        System.out.println("student Name:"+name);
+        System.out.println("student id:"+id);
+        
+    }
+    
+}
